23a7b4b34f87fe1c452d3994e3a79e01e519bdad
Cory Brighton <corysairplane@gmail.com>
2018-12-03 13:33:04 -0700

    first commit

--- src/store.js
+++ src/store.js
@@ -5,12 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    target: {
+      name: 'Cory',
+      number: 0
+    }
   },
   mutations: {
-
+    //first value on every mutation is state
+    attack(state, payload) {
+      state.target.number += payload
+    }
   },
   actions: {
 
+    //first parameter on every action is {commit, dispatch}
+    attack({ commit, dispatch }, payload) {
+      commit('attack', payload)
+    }
   }
 })
